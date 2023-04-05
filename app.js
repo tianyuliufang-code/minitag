34048e1851ce5267dc51e1f39be48ce2fe45138f
tianyuliufang-code <3321593926@qq.com>
2023-04-05 21:11:03 +0800

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+const defaultTime = {
+  defaultWorkTime: 30,
+  defaultRestTime: 5
+}
 App({
   globalData:{},//用来保存全局变量
   onLaunch:function(){
+    let workTime = wx.getStorageSync('workTime')
+    let restTime = wx.getStorageSync('restTime')
+    
+    if (!workTime) {
+      wx.setStorage({
+        key: 'workTime',
+        data: defaultTime.defaultWorkTime
+      })
+    }
+    if (!restTime) {
+      wx.setStorage({
+        key: 'restTime',
+        data: defaultTime.defaultRestTime
+      })
+    }
     //获取一些系统信息并保存为全局变量
     wx.getSystemInfo({
       success:res=>{
@@ -10,5 +29,6 @@ App({
         this.globalData.CustomBar = custom.bottom + custom.top-res.statusBarHeight
       }
     })
+
   }
 })
---
+++ pages/setmore/setmore.js
@@ -0,0 +1,122 @@
+// pages/setmore/setmore.js
+Page({
+  navigateBack: function() {
+    wx.navigateBack({
+      delta: 1   // 返回的页面数，如果 delta 等于 1，则返回上一个页面
+    })
+  },  
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        vibison: false,
+        appId: "wxa326142a4ebb6147",   
+        topTips: false,
+        tophide: false,
+    },
+
+    changeWorkTime: function (e) {
+        wx.setStorage({
+          key: 'workTime',
+          data: e.detail.value
+        })
+      },
+      changeRestTime: function (e) {
+        wx.setStorage({
+          key: 'restTime',
+          data: e.detail.value
+        })
+      },
+      changevib: function (e) { //震动控制
+        wx.setStorage({
+          key: 'vibison',
+          data: e.detail.value
+        })
+    if(e.detail.value==true){
+      this.setData({
+        topTips: true
+    });
+    setTimeout(() => {
+      this.setData({
+        tophide: true
+    });
+    setTimeout(() => {
+        this.setData({
+            topTips: false,
+            tophide: false,
+        });
+    }, 300);
+    }, 1200);
+    }
+      },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+      // // 监听返回事件
+      //     wx.navigateBack({
+      //       delta: 1   // 返回的页面数，如果 delta 等于 1，则返回上一个页面
+      //     })
+
+        wx.setNavigationBarTitle({
+          title: '设置'
+        })
+        this.setData({
+          workTime: wx.getStorageSync('workTime'),
+          restTime: wx.getStorageSync('restTime'),
+          vibison: wx.getStorageSync('vibison')
+        })
+
+      },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
